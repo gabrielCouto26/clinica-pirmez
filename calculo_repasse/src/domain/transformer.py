75b71b69704138d9bc246ca0c6e6115402151c0d
Gabriel Couto <gabrielcouto26@gmail.com>
2024-04-30 00:27:28 -0300

    implementa transformer inicial

--- calculo_repasse/src/domain/transformer.py
+++ calculo_repasse/src/domain/transformer.py
@@ -1,10 +1,26 @@
 import pandas as pd
 
 
+PROCEDURES_SHARE = {
+    'procedimento1': 30 / 100,
+    'procedimento2': 20 / 100,
+    'procedimento3': 40 / 100,
+}
+
+
 class Transformer:
     def __init__(self) -> None:
         pass
 
     def calculate_share(self, data: pd.DataFrame) -> pd.DataFrame:
+        assert not data.empty, 'Invalid data provided: empty'
         assert isinstance(data, pd.DataFrame)
-        return data
+
+        data['repasse'] = pd.Series()
+        data['repasse'] = (
+            data['procedimento'].map(PROCEDURES_SHARE)
+            * data['valor'])
+
+        final = data.groupby(['medico']).agg({'repasse': 'sum'})
+
+        return final
