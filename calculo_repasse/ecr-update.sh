ac0cf53cfcfc9457fa22385d6d172e538d496694
Gabriel Couto <gabrielcouto26@gmail.com>
2024-05-03 20:10:57 -0300

    implementa script bash para atualizar imagem Docker no ECR

---
+++ calculo_repasse/ecr-update.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+aws --profile ClinicaPirmezTerraform ecr get-login-password --region us-east-1 | \
+    docker login --username AWS --password-stdin 415953478593.dkr.ecr.us-east-1.amazonaws.com
+
+docker build -t calculo-repasse-lambda:latest .
+docker tag calculo-repasse-lambda:latest 415953478593.dkr.ecr.us-east-1.amazonaws.com/calculo-repasse-lambda-container:latest
+docker push 415953478593.dkr.ecr.us-east-1.amazonaws.com/calculo-repasse-lambda-container:latest
+
+echo "Imagem Docker atualizada!"
