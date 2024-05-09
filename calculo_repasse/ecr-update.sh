f862d532d7813aebefd88c70325a867e2b311b44
Gabriel Couto <gabrielcouto26@gmail.com>
2024-05-08 23:01:28 -0300

    atualiza lambda-function depois de subir a nova imagem

--- calculo_repasse/ecr-update.sh
+++ calculo_repasse/ecr-update.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
-aws --profile ClinicaPirmezTerraform ecr get-login-password --region us-east-1 | \
+aws --profile ClinicaPirmezTerraform \
+    ecr get-login-password | \
     docker login --username AWS --password-stdin 415953478593.dkr.ecr.us-east-1.amazonaws.com
 
 docker build -t calculo-repasse-lambda:latest . -f Dockerfile.lambda
@@ -8,3 +9,10 @@ docker tag calculo-repasse-lambda:latest 415953478593.dkr.ecr.us-east-1.amazonaw
 docker push 415953478593.dkr.ecr.us-east-1.amazonaws.com/calculo-repasse-lambda-container:latest
 
 echo "Imagem Docker atualizada!"
+
+aws --profile ClinicaPirmezTerraform \
+    lambda update-function-code \
+    --function-name calculo-repasse-lambda \
+    --image-uri 415953478593.dkr.ecr.us-east-1.amazonaws.com/calculo-repasse-lambda-container:latest
+
+echo "Função Lambda atualizada!"
