c8477577e7ca6ea7ae1f3871906f43fbcf5311e4
Gabriel Couto <gabrielcouto26@gmail.com>
2024-05-12 22:38:19 -0300

    corrige nome do método de FileStorage

--- calculo_repasse/src/ports/__init__.py
+++ calculo_repasse/src/ports/__init__.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 
 
 class FileStorage(Protocol):
-    def fetch(self, path: str, *args) -> DataFrame:
+    def extract(self, path: str, *args) -> DataFrame:
         pass
 
     def load(self, file_path: str, dest: str, *args) -> None:
