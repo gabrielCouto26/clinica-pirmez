570c595b4636908ceab48f4c3816e4b25ef04c67
Gabriel Couto <gabrielcouto26@gmail.com>
2024-05-12 21:55:57 -0300

    adiciona helper timestamp em métodos de storage

--- calculo_repasse/src/domain/storage.py
+++ calculo_repasse/src/domain/storage.py
@@ -1,13 +1,16 @@
 from pandas import DataFrame
 from src.ports import FileStorage
+from src.libs.helpers import timestamp
 
 
 class Storage:
     def __init__(self, storage: FileStorage) -> None:
         self.storage = storage
 
+    @timestamp
     def extract(self, bucket: str, key: str) -> DataFrame:
         return self.storage.extract(bucket, key)
 
+    @timestamp
     def load(self, file_path: str, dest: str, *args) -> None:
         self.storage.load(file_path, dest, *args)
